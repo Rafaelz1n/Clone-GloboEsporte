38e5b0702a5d5ff257875c1012202247b15c1069
Rafael Magalhãee <rafael.a7x.miranda@gmail.com>
2024-02-17 03:34:41 -0300

    first commit

---
+++ src/components/content/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.css";
+
+export default function Content() {
+  return (
+    <div className="content-principal">
+      <h1>Rafael</h1>
+    </div>
+  );
+}
---
+++ src/components/subHeader/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { FiShield } from "react-icons/fi";
+
+import "./style.css";
+
+export default function SubHeader() {
+  return (
+    <div className="sub-header">
+      <div className="sub-header-item">
+        <nav className="nav-icons-header">
+          <FaBars size={22} />
+          <h3>MENU</h3>
+          <span></span>
+          <FiShield size={26} />
+          <h3>TIMES</h3>
+        </nav>
+      </div>
+      <div className="sub-header-item">
+        <h1>globoesporte.com</h1>
+      </div>
+      <div className="sub-header-item">
+        <div className="input-sub-header">
+          <input type="text" placeholder="BUSCAR" />
+        </div>
+      </div>
+    </div>
+  );
+}
